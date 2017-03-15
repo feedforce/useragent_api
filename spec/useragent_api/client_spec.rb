90e9d0508ddd3890ab3bf67ee91f7e9790757266
Takashi Masuda <masutaka@feedforce.jp>
2017-03-15 16:28:44 +0900

    Refactor spec

--- spec/useragent_api/client_spec.rb
+++ spec/useragent_api/client_spec.rb
@@ -14,10 +14,27 @@ describe UseragentApi::Client do
   end
 
   describe '#parse' do
+    shared_examples_for 'UseragentAPI' do
+      let(:request_uri) { 'https://useragentapi.com/api/v4/json/%s/%s' % [api_key, CGI.escape(user_agent)] }
+
+      before do
+        stub_request(:get, request_uri).to_return(
+          status: stub_status, body: stub_body
+        )
+      end
+
+      it 'is valid response' do
+        expect(client.parse(user_agent)).to match_array valid_response
+      end
+    end
+
     let(:client) { UseragentApi::Client.new(api_key) }
+    let(:api_key) { '01234abc' }
+    let(:user_agent) { 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' }
 
     context 'given registered API key' do
-      let(:api_key) { '01234abc' }
+      let(:stub_status) { 200 }
+      let(:stub_body) { valid_response.to_json }
       let(:valid_response) do
         {
           'data' => {
@@ -31,24 +48,14 @@ describe UseragentApi::Client do
           }
         }
       end
-      let(:stub_status) { 200 }
-      let(:stub_body) { valid_response.to_json }
-      let(:user_agent) { 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' }
-      let(:request_uri) { 'https://useragentapi.com/api/v4/json/%s/%s' % [api_key, CGI.escape(user_agent)] }
 
-      before do
-        stub_request(:get, request_uri).to_return(
-          status: stub_status, body: stub_body
-        )
-      end
-
-      it 'is valid response' do
-        expect(client.parse(user_agent)).to match_array valid_response
-      end
+      it_behaves_like 'UseragentAPI'
     end
 
     context 'given unregistered API key' do
       let(:api_key) { 'unregistered' }
+      let(:stub_status) { 400 }
+      let(:stub_body) { valid_response.to_json }
       let(:valid_response) do
         {
           'error' => {
@@ -57,38 +64,16 @@ describe UseragentApi::Client do
           }
         }
       end
-      let(:stub_status) { 400 }
-      let(:stub_body) { valid_response.to_json }
-      let(:user_agent) { 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' }
-      let(:request_uri) { 'https://useragentapi.com/api/v4/json/%s/%s' % [api_key, CGI.escape(user_agent)] }
 
-      before do
-        stub_request(:get, request_uri).to_return(
-          status: stub_status, body: stub_body
-        )
-      end
-
-      it 'is valid response' do
-        expect(client.parse(user_agent)).to match_array valid_response
-      end
+      it_behaves_like 'UseragentAPI'
     end
 
     context 'when a server returns a broken response' do
-      let(:api_key) { '01234abc' }
       let(:stub_status) { 500 }
       let(:stub_body) { '<b>Fatal error</b>' }
-      let(:user_agent) { 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' }
-      let(:request_uri) { 'https://useragentapi.com/api/v4/json/%s/%s' % [api_key, CGI.escape(user_agent)] }
+      let(:valid_response) { {} }
 
-      before do
-        stub_request(:get, request_uri).to_return(
-          status: stub_status, body: stub_body
-        )
-      end
-
-      it 'is valid response' do
-        expect(client.parse(user_agent)).to eq({})
-      end
+      it_behaves_like 'UseragentAPI'
     end
   end
 end
